20b1ebf53bf465752ab49bf186a83458c487c36f
Timon <timon@diodes-delight.com>
2023-12-14 05:57:39 +0100

    Add adafruit_adv7611 for prototyping

---
+++ ports/raspberrypi/boards/adafruit_adv7611/mpconfigboard.h
@@ -0,0 +1,7 @@
+#define MICROPY_HW_BOARD_NAME "Adafruit ADV7611"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO12)
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO17)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO18)
